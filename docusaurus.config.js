e0727d8ff078a7e725979414dd3a5f4e60daedc7
Leon Bohmann <mail@leonbohmann.de>
2024-08-26 15:21:33 +0200

    mod

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/icon.ico',
 
   // Set the production url of your site here
-  url: 'https://leonbohmann.github.io/fracsuite-docs/',
+  url: 'https://leonbohmann.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/fracsuite-docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
